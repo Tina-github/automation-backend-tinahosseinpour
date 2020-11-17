b654dabd521d264fa0a535cf50600bc18a6a4b82
Tina Hsp <farzaneh.hosseinpour@gmail.com>
2020-11-17 12:35:17 +0100

    created 4 testcases

--- backend-tests/cypress/helpers/roomHelpers.js
+++ backend-tests/cypress/helpers/roomHelpers.js
@@ -1,13 +1,15 @@
 const ENDPOINT_POST_ROOM = 'http://localhost:3000/api/room/new'
 const ENDPOINT_GET_ROOMS = 'http://localhost:3000/api/rooms'
-const ENDPOINT_PUT_ROOM = 'http://localhost:3000/api/room/2'
+const ENDPOINT_PUT_ROOM = 'http://localhost:3000/api/room/'
 const ENDPOINT_GET_ROOM = 'http://localhost:3000/api/room/'
+const faker = require('faker')
 
 function createRoomPayload(){
-    
+    const fakeid = faker.random.number({min:2000 , max:1000000})
 
     const payload = 
-    {"features":["balcony"],
+    {"id":fakeid,
+        "features":["balcony"],
     "category":"single",
     "number":"4",
     "floor":"2",
@@ -18,10 +20,12 @@ function createRoomPayload(){
 }
  
 function editRoomPayload(){
-    
+    const fakeid = faker.random.number({min:2000 , max:1000000})
 
     const payload = 
-    {"features":["balcony"],
+
+    {"id":fakeid,
+        "features":["balcony"],
     "category":"single",
     "number":"5",
     "floor":"5",
@@ -124,13 +128,24 @@ function createRoomRequest(cy){
 }
 
 function editRoomRequest(cy){
-    cy.authenticateSession().then((response =>{
+cy.authenticateSession().then((response =>{
         let RoomPayload = editRoomPayload() 
+    cy.request({
+        method: "GET",
+        url: ENDPOINT_GET_ROOMS,
+        headers:{
+            'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+            'Content-Type': 'application/json'
+        },
+    }).then((response =>{
+        const responseAsString = JSON.stringify(response)
+        let lastId = response.body[response.body.length -1].id
+    
         
         // post request to create a room
         cy.request({
             method: "PUT",
-            url: ENDPOINT_PUT_ROOM,
+            url: ENDPOINT_PUT_ROOM+lastId,
             headers:{
                 'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
                 'Content-Type': 'application/json'
@@ -143,6 +158,7 @@ function editRoomRequest(cy){
 
         getRequestAllRoomsWithAssertion(cy,RoomPayload.features, RoomPayload.category, RoomPayload.number ,RoomPayload.floor , RoomPayload.available , RoomPayload.price )
     }))
+}))
 }
 function createRoomRequestAndDelete(cy){
     cy.authenticateSession().then((response =>{
